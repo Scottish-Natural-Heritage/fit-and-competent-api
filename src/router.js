46b010a7043c7d7ccb4322c99676fd238e6478a1
John Moore <john.moore@snh.gov.uk>
2020-11-12 16:09:07 +0000

    Add route to router
    Issue: Scottish-Natural-Heritage/Licensing#411

--- src/router.js
+++ src/router.js
@@ -1,10 +1,29 @@
 import express from 'express';
+import config from './config/app.js';
 
 const router = express.Router();
+
+import Application from './controllers/application.js';
 
 // `/health` is a simple health-check end-point to test whether the service is up.
 router.get('/health', async (request, response) => {
   response.status(200).send({message: 'OK'});
+});
+
+// Allow an API consumer to allocate a new application number.
+router.post('/applications', async (request, response) => {
+  const baseUrl = new URL(
+    `${request.protocol}://${request.hostname}:${config.port}${request.originalUrl}${
+      request.originalUrl.endsWith('/') ? '' : '/'
+    }`
+  );
+
+  try {
+    const newId = await Application.create();
+    response.status(201).location(new URL(newId, baseUrl)).send();
+  } catch (error) {
+    response.status(500).send({error});
+  }
 });
 
 export {router as default};
